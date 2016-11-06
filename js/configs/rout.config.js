66b1061b40b0e5b4314d80884fc6aba0d4d15010
SergeyVoloshchuk <sergeyvolochuc@gmail.com>
2016-11-06 15:30:56 +0300

    add config routing

---
+++ js/configs/rout.config.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+    angular
+        .module("ui.router")
+        .config(RoutingConfig);
+
+    function RoutingConfig($stateProvider) {
+        var state = {
+            name: 'main',
+            url: '',
+            template: 'hello'
+        }
+
+        var profState = {
+            name: 'profile',
+            url: '/profile',
+            templateUrl: '../../templates/prof.html'
+        }
+
+        var messageState = {
+            name: 'messages',
+            url: '/messages',
+            templateUrl: "../../templates/message.html"
+        }
+        var listState = {
+            name: 'list',
+            url: '/list',
+            templateUrl: "../../templates/list.html"
+        }
+        $stateProvider.state(state);
+        $stateProvider.state(profState);
+        $stateProvider.state(messageState);
+        $stateProvider.state(listState);
+    }
+
+})();
